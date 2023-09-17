a32d98768c8385b7e09ed22dde8a7667e1468b8f
LevanPangani5 <levanfangani55@gmail.com>
2023-09-18 02:24:29 +0400

    finishes menue , categories ,singleProduct

--- src/app/components/Offer.tsx
+++ src/app/components/Offer.tsx
@@ -4,7 +4,7 @@ import CountDown from './CountDown'
 
 const Offer = () => {
   return (
-    <div className="bg-black h-screen flex-col md:flex-row md:justify-between md:bg-[url('/')]">
+    <div className="bg-black h-screen flex flex-col md:flex-row md:justify-between md:bg-[url('/offerBg.png')] md:h-[70vh]">
       {/*TEXT CONTAINER */}
         <div className='flex-1 flex flex-col justify-center items-center text-center gap-8 p-6'>
             <h1 className='text-white text-5xl font-bold xl:text-6xl'>Delicious Burger & Frernch fry</h1>
@@ -16,7 +16,7 @@ const Offer = () => {
         </div>
         {/*IMAGE CONTAINER */}
         <div className='relative flex-1 w-full md:h-full'>
-           <Image src="/" alt="" fill className='object-contain'/>
+           <Image src="/offerProduct.png" alt="" fill className='object-contain'/>
         </div>
     </div>
   )
--- src/app/menu/[Category]/page.tsx
+++ src/app/menu/[Category]/page.tsx
@@ -1,8 +1,33 @@
+import { pizzas } from '@/data';
+import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react'
 
 const CategoryPage = () => {
   return (
-    <div>CategoryPage</div>
+    <div className='flex flex-wrap text-red-500'>
+      {pizzas.map(item=>(
+        <Link 
+        href={`/product/${item.id}`} 
+        key={item.id}
+        className='group even:bg-fuchsia-50 w-full h-[60vh] border-r-2 border-b-2 sm:w-1/2 lg:w-1/3 border-red-500 p-4 flex flex-col justify-between'
+        > 
+        {/*image container*/}
+          {item.img && (
+          <div className='relative h-[80%]'>
+            <Image src={item.img} alt="" fill className='object-contain'/>
+          </div> 
+            )}
+        {/*text container */}
+        <div className=' flex items-center justify-between '>
+          <h1 className=' text-2xl uppercase p-2'>{item.title}</h1>
+          <h2 className='group-hover:hidden text-xl'>${item.price}</h2>
+          <button className='hidden group-hover:block uppercase bg-red-500 text-white p-2 rounded-md'>Add to cart</button>
+        </div>
+          
+        </Link>
+      ))}
+    </div>
   )
 }
 
